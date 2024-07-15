928d1e8f666d2a6c4a0de7a102fead6219caf528
Masie Junior Seremu <seremu3@gmail.com>
2024-07-15 17:40:57 +0200

    Created reusable widget

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -3,12 +3,24 @@ import 'package:flutter/material.dart';
 Color primary = const Color(0xFF687daf);
 
 class AppStyles {
+  //Colours
   static Color primaryColor = primary;
   static Color textColor = const Color(0xFF3b3b3b);
   static Color bgColor = const Color(0xFFeeedf2);
 
+//TextStyles
+  static TextStyle textStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: textColor,
+  );
   static TextStyle headLineStyle1 = TextStyle(
     fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
+  static TextStyle headLineStyle2 = TextStyle(
+    fontSize: 21,
     fontWeight: FontWeight.bold,
     color: textColor,
   );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/base/widgets/app_double_text.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -77,9 +78,16 @@ class _HomeScreenState extends State<HomeScreen> {
                     ],
                   ),
                 ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const AppDoubleText(
+                  bigText: "Upcoming Flights",
+                  smallText: "View all",
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
